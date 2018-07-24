0231ea2599852d5eef3d08d4ba651f604d185cad
Raghu Nandan Avula <raghu.avula@ucf.edu>
2018-07-24 16:23:49 -0400

    Update name and email address in the footer. - RISFTR-25
    Signed-off-by: Raghu Nandan Avula <raghu.avula@ucf.edu>

--- js/ris_footer.js
+++ js/ris_footer.js
@@ -1,4 +1,11 @@
-// merge css files in the future (?)
+/*
+steps for adding this footer to a webapp or website
+	0. an example can be found in the file "ris_footer_example.html" located in the current directory
+	1. include this file along with jQuery (as well as certain google fonts)
+	2. call the loadFooter() function
+	3. create a div where you'd like the footer to be with the id = "ucf-orc-footer";
+ */
+
 var styles_link = "https://header.dev.research.ucf.edu/styles/footer.css";
 var styles_link_mobile = "https://header.dev.research.ucf.edu/styles/footer_mobile.css";
 var images_source_dev = "https://header.dev.research.ucf.edu/images/";
@@ -22,10 +29,10 @@ function loadFooter() {
 		'<div class="ucf-orc-footer-main">' +
 		    '<div class="ucf-orc-footer-sectionwrapper centered">' +
 		    	'<ul id="no-bullets">' +
-		            '<li class="ucf-orc-footer-serif"><span style="color: white">&copy;</span> <a class="ucf-white white-underline" style="color: white;" href="http://www.research.ucf.edu" target="_blank">UCF Office of Research &amp; Commercialization</a></li>' +
-		            '<li class="ucf-orc-footer-ribbon"><a class="ucf-white gold-underline" href="http://it.research.ucf.edu" target="_blank"><h5 class="ucf-orc-footer-sans-serif">Developed by Research Information Systems</h5></a></li>' +
+		            '<li class="ucf-orc-footer-serif"><span style="color: white">&copy;</span> <a class="ucf-white white-underline" style="color: white;" href="http://www.research.ucf.edu" target="_blank">UCF Office of Research & Commercialization</a></li>' +
+		            '<li class="ucf-orc-footer-ribbon"><a class="ucf-white gold-underline" href="http://it.research.ucf.edu" target="_blank"><h5 class="ucf-orc-footer-sans-serif">Developed by Graduate and Research Information Technology</h5></a></li>' +
 		            '<li class="ucf-orc-footer-ribbon ucf-orc-footer-icons">' +
-		                '<img src="'+ images_source_dev + 'ldg_mail.png" width="20" height="12"/>&nbsp;<a id="ucf-white" href="mailto:risservicedesk@ucf.edu">risservicedesk@ucf.edu</a>&nbsp; ' +
+		                '<img src="'+ images_source_dev + 'ldg_mail.png" width="20" height="12"/>&nbsp;<a id="ucf-white" href="mailto:GRITservicedesk@ucf.edu">GRITservicedesk@ucf.edu</a>&nbsp; ' +
 		                '<span style="margin-top:5px;"><img src="'+ images_source_dev + 'ldg_phone.png" width="16" height="25"/>&nbsp;407.823.5500</span>' +
 		            '</li>' +
 		        '</ul>' +
