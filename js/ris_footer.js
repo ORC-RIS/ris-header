001186acae6e238ec3d46c6e6d957cce95082323
Mark Durbin <mark.durbin@ucf.edu>
2026-03-31 07:46:05 -0400

    Wcag (#7)
    * Added black color to text to override style from PARIS.
    
    * Added alt attributes to img tags
    
    * Increase element size
    
    * Changed heading tag to span
    
    * Changed height to padding
    
    * Replace <center> tags
    
    * Add role to list
    
    * Fix aria role

--- js/ris_footer.js
+++ js/ris_footer.js
@@ -28,13 +28,13 @@ function loadFooter() {
 	$(document).ready(function() {
 		$('#ucf-orc-footer').html(
 		'<div class="ucf-orc-footer-main">' +
-			'<div class="ucf-orc-footer-sectionwrapper centered">' +
+			'<div class="ucf-orc-footer-sectionwrapper centered" role="navigation">' +
 				'<ul id="no-bullets">' +
 					'<li class="ucf-orc-footer-serif"><span style="color: white">&copy;</span> <a class="ucf-white white-underline" style="color: white;" href="http://www.research.ucf.edu" target="_blank">UCF Office of Research</a></li>' +
-					'<li class="ucf-orc-footer-ribbon"><a class="ucf-white gold-underline" href="http://it.research.ucf.edu" target="_blank"><h5 class="ucf-orc-footer-sans-serif">Developed by Graduate and Research Information Technology</h5></a></li>' +
+					'<li class="ucf-orc-footer-ribbon"><a style="display: inline-block; padding: 1.25rem 0rem" class="ucf-white gold-underline" href="http://it.research.ucf.edu" target="_blank"><span class="ucf-orc-footer-sans-serif">Developed by Graduate and Research Information Technology</span></a></li>' +
 					'<li class="ucf-orc-footer-ribbon ucf-orc-footer-icons">' +
-						'<img src="'+ images_source + 'ldg_mail.png" width="20" height="12"/>&nbsp;<a id="ucf-white" href="mailto:GRITservicedesk@ucf.edu">GRITservicedesk@ucf.edu</a>&nbsp; ' +
-						'<span style="margin-top:5px;"><img src="'+ images_source + 'ldg_phone.png" width="16" height="25"/>&nbsp;407.823.5500</span>' +
+						'<img src="'+ images_source + 'ldg_mail.png" width="20" height="12" alt="Email icon"/>&nbsp;<a id="ucf-white" href="mailto:GRITservicedesk@ucf.edu">GRITservicedesk@ucf.edu</a>&nbsp; ' +
+						'<span style="margin-top:5px;"><img src="'+ images_source + 'ldg_phone.png" width="16" height="25" alt="Phone icon"/>&nbsp;407.823.5500</span>' +
 					'</li>' +
 				'</ul>' +
 			'</div>' +
